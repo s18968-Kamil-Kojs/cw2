1629c620485da9541e2d0fce84c82c48d84a176d
Kamil Kojs <kamil.kojs@gmail.com>
2020-03-09 16:57:33 +0100

    third commit

--- APBD02v2/APBD02v2/Models/Student.cs
+++ APBD02v2/APBD02v2/Models/Student.cs
@@ -1,7 +1,42 @@
 ﻿using System;
+using System.Text.Json.Serialization;
+using System.Xml.Serialization;
+
 namespace APBD02v2.Models {
+
     public class Student {
+
         public Student() {
         }
+
+        //prop + tabx2
+
+        [XmlAttribute(AttributeName = "Imie")]
+        public string Imie { get; set; }
+
+        [XmlAttribute(AttributeName = "Nazwisko")]
+        //[JsonPropertyName("LastName")]
+        public string Nazwisko { get; set; }
+
+        [XmlAttribute(AttributeName = "kierunek")]
+        public string Kierunek { get; set; }
+
+        [XmlAttribute(AttributeName = "tryb")]
+        public string Tryb { get; set; }
+
+        [XmlAttribute(AttributeName = "jakas liczba")]
+        public string Liczba { get; set; }
+
+        [XmlAttribute(AttributeName = "data urodzenia")]
+        public string Urodz { get; set; }
+
+        [XmlAttribute(AttributeName = "email")]
+        public string Email { get; set; }
+
+        [XmlAttribute(AttributeName = "imie matki")]
+        public string ImieMatki { get; set; }
+
+        [XmlAttribute(AttributeName = "imie ojca")]
+        public string ImieOjca { get; set; }
     }
 }
--- APBD02v2/APBD02v2/Program.cs
+++ APBD02v2/APBD02v2/Program.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Xml.Serialization;
 using APBD02v2.Models;
 
 namespace APBD02v2 {
     class Program {
         static void Main(string[] args) {
             string path = @"Data/dane.csv";
+            var list = new List<Student>();
 
             //Wczytywanie pliku
             var fi = new FileInfo(path);
@@ -14,19 +16,23 @@ namespace APBD02v2 {
 
                 string line = null;
                 while ((line = stream.ReadLine()) != null) {
+                    string[] kolumny = line.Split(',');
                     Console.WriteLine(line);
                 }
             }
             //stream.Dispose();
 
             //XML
-            var list = new List<Student>();
             var st = new Student {
                 Imie = "Jan",
                 Nazwisko = "Kowalski",
                 Email = "kowalski@wp.pl"
             };
             list.Add(st);
+
+            FileStream writer = new FileStream(@"data.xml", FileMode.Create);
+            XmlSerializer serializer = new XmlSerializer(typeof(List<Student>), new XmlRootAttribute("uczelnia"));
+            serializer.Serialize(writer, list);
         }
     }
 }
