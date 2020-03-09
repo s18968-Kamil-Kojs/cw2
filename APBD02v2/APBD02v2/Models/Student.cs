3604c9a40593eca5cc7cbc323ce645081945a839
Kamil Kojs <kamil.kojs@gmail.com>
2020-03-09 16:26:33 +0100

    first commit

---
+++ APBD02v2/APBD02v2/Models/Student.cs
@@ -0,0 +1,7 @@
+﻿using System;
+namespace APBD02v2.Models {
+    public class Student {
+        public Student() {
+        }
+    }
+}
--- APBD02v2/APBD02v2/Program.cs
+++ APBD02v2/APBD02v2/Program.cs
@@ -1,9 +1,24 @@
 ﻿using System;
+using System.IO;
 
 namespace APBD02v2 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello World!");
+            string path = @"Data/dane.csv";
+
+            //Wczytywanie pliku
+            var fi = new FileInfo(path);
+            using (var stream = new StreamReader(fi.OpenRead())) {
+
+                string line = null;
+                while ((line = stream.ReadLine()) != null) {
+                    Console.WriteLine(line);
+                }
+            }
+            //stream.Dispose();
+
+            //XML
+
         }
     }
 }
